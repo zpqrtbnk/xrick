6a3e86c73ca2f2fc634ddcf38e102e7b9d6324c8
Stephan <stephane@umbraco.dk>
2019-05-12 11:23:59 +0200

    Import EmSdk build script

---
+++ build/emsdk/build.sh
@@ -0,0 +1,42 @@
+# xrick emscripten build script
+
+# configure paths
+PATH_PYTHON=/c/python27-x64
+PATH_EMSDK="/d/d/EmSdk"
+PATH_XRICK="/d/d/Rick Dangerous/wip/xrick-vs19"
+
+# ensure Python
+echo Adding Python directory to PATH:
+echo PATH += $PATH_PYTHON
+PATH=$PATH_PYTHON:$PATH
+echo
+
+# configure emsdk
+echo Configuring EmSdk...
+source "$PATH_EMSDK/emsdk_env.sh"
+
+SRC="$PATH_XRICK/xrick/src"
+INC="$PATH_XRICK/xrick/include"
+DATA="$PATH_XRICK/data"
+
+echo Compiling...
+for f in "$SRC"/*.c; \
+  do emcc "$f" -o $(basename -- "${f%.c}").bc \
+    -I "$INC" \
+    -s USE_SDL=2 \
+    -D NOZLIB \
+    -O2 \
+    ; \
+done
+echo
+
+echo Linking...
+# -o .wasm .html .js
+emcc *.bc -o xrick.js \
+  -s USE_SDL=2 \
+  -s FORCE_FILESYSTEM=1 \
+  -O2 \
+  --preload-file "$DATA@/data"
+echo
+
+echo Done.
