9c090b944255917b3e917b9e5859596dbd5b004c
Stephan <stephane@umbraco.dk>
2019-05-29 17:48:13 +0200

    Player script

--- build/emsdk/player.js
+++ build/emsdk/player.js
@@ -128,84 +128,24 @@ function isIPx() {
     || (navigator.platform.indexOf("iPod") != -1);
 }
 
-function setClickStart() {
-    $('#xrick span').html('click to play');
-    $('#xrick').on('click', function () {
-        
-        // unlock iPhone/iPad audio
-        if (isIPx())
-            startAudio();
+function switchToCanvasAndRun() {
+    // switch to canvas
+    $('#xrick').hide();
+    $('#canvas').css('display', 'block'); // show() would use 'inline' on a canvas
+    $('#canvas').on('blur', function() { console.log('pause'); });
 
-        // switch to canvas
-        $('#xrick').hide();
-        $('#canvas').css('display', 'block'); // show() would use 'inline' on a canvas
-        $('#canvas').on('blur', function() { console.log('pause'); });
-
-        // run
-        Module['callMain'](Module['arguments']);
-    });
+    // run
+    Module['callMain'](Module['arguments']);
 }
 
-function sendKey(down, code) {
-    var e = document.createEventObject ? document.createEventObject() : document.createEvent("Events");
-    if (e.initEvent) e.initEvent(down ? 'keydown' : 'keyup', true, true); // bubbles, cancelable
-
-    e.keyCode = code;
-    e.which = code;
-    //e.charCode = charCode;
-    e.charCode = 0;
-    
-    //console.log((down?'+':'-')+code);
-    Module['canvas'].dispatchEvent(e);
-}
-
-// get arguments from querystring, and add missing arguments
-var args = getArgs();
-setArg(args, 'data', '/data');
-setArg(args, 'keys', 'W-X-O-K-SPACE');
-setArg(args, 'zoom', '1');
-Module['arguments'] = args;
-
-Module['locateFile'] = function (path, prefix) {
-    // if it's the data file, use cdn
-    if (path.endsWith(".data")) return "http://www1.xrick.net/" + path;
-    // otherwise, use the default, the prefix (JS file's dir) + the path
-    return prefix + path;
-}
-
-// no initial run!
-Module['noInitialRun'] = true;
-
-Module.setStatus('Downloading...');
-
-if (isIPx()) {
-    
-    function setKey(id, code) {
-        $(id)
-        .on('touchstart', function(e) {
-            e.preventDefault();
-            sendKey(true, code);
-        })
-        .on('touchend touchcancel', function(e) {
-            e.preventDefault();
-            sendKey(false, code);
-        });
-    }
+function initializeTouch() {
     
     // layout
     $('#content').css('margin-top', '4px');
-    $('#controls').show();
+    $('#controls2').css('display', 'flex'); // show() would use 'inline' on a div
     
-    // initialize touch controls
+    // initialize controls
     setKey('#control_fire', 32);
-    setKey('#control_mute', 115);
-    setKey('#control_snd1', 116);
-    setKey('#control_snd2', 117);
-    setKey('#control_cheat1', 118);
-    setKey('#control_cheat2', 119);
-    setKey('#control_cheat3', 120);
-    setKey('#control_end', 69);
-    setKey('#control_pause', 80);
     
     var cx = 0;
     var cy = 0;
@@ -225,25 +165,25 @@ if (isIPx()) {
         if (x > cx + xo) {
             if (!kr) {
                 kr = true;
-                sendKey(true, 88);
+                sendKey(true, 68);
             }
         }
         else {
             if (kr) {
                 kr = false;
-                sendKey(false, 88);
+                sendKey(false, 68);
             }
         }
         if (x < cx - xo) {
             if (!kl) {
                 kl = true;
-                sendKey(true, 87);
+                sendKey(true, 83);
             }
         }
         else {
             if (kl) {
                 kl = false;
-                sendKey(false, 87);
+                sendKey(false, 83);
             }
         }
         if (y > cy + yo) {
@@ -276,18 +216,15 @@ if (isIPx()) {
     .on('touchstart', function(e) {
         e.preventDefault();
         
-        var f = 8;
         var pad = $(this);
         var ofs = pad.offset();
         var w = pad.innerWidth();
         var h = pad.innerHeight();
-        var s = w;
-        if (h < s) s = h;
         
         cx = ofs.left + w / 2;
         cy = ofs.top + h / 2;
-        xo = s / f;
-        yo = s / f;
+        xo = w / 6;
+        yo = h / 4;
 
         var touch = e.targetTouches[0];
         onMove(touch.pageX, touch.pageY);
@@ -302,5 +239,80 @@ if (isIPx()) {
         onMove(cx, cy);
     });    
 }
+
+function setClickStart() {
+    $('#xrick span').html('click/touch to play');
+    
+    // order: touchstart touchmove touchend mousemove mousedown mouseup click
+    $('#xrick')
+    .on('touchstart', function () {
+        initializeTouch();
+    })
+    .on('click', function () {
+        
+        // iPhone/iPad: unlock audio + resize
+        if (isIPx()) {
+            startAudio();
+            $('#content').css('margin-top', '4px');
+        }
+        
+        // go
+        switchToCanvasAndRun();
+    });
+}
+
+function sendKey(down, code) {
+    var e = document.createEventObject ? document.createEventObject() : document.createEvent("Events");
+    if (e.initEvent) e.initEvent(down ? 'keydown' : 'keyup', true, true); // bubbles, cancelable
+
+    e.keyCode = code;
+    e.which = code;
+    //e.charCode = charCode;
+    e.charCode = 0;
+    
+    //console.log((down?'+':'-')+code);
+    Module['canvas'].dispatchEvent(e);
+}
+
+function setKey(id, code) {
+    $(id)
+    .on('touchstart mousedown', function(e) {
+        e.preventDefault();
+        sendKey(true, code);
+    })
+    .on('touchend touchcancel mouseup', function(e) {
+        e.preventDefault();
+        sendKey(false, code);
+    });
+}
+
+// get arguments from querystring, and add missing arguments
+var args = getArgs();
+setArg(args, 'data', '/data');
+setArg(args, 'keys', 'S-D-O-K-SPACE');
+setArg(args, 'zoom', '1');
+Module['arguments'] = args;
+
+Module['locateFile'] = function (path, prefix) {
+    // if it's the data file, use cdn
+    if (path.endsWith(".data")) return "http://www1.xrick.net/" + path;
+    // otherwise, use the default, the prefix (JS file's dir) + the path
+    return prefix + path;
+}
+
+// no initial run!
+Module['noInitialRun'] = true;
+
+Module.setStatus('Downloading...');
+
+// initialize controls
+setKey('#control_mute', 115);
+setKey('#control_snd1', 116);
+setKey('#control_snd2', 117);
+setKey('#control_cheat1', 118);
+setKey('#control_cheat2', 119);
+setKey('#control_cheat3', 120);
+setKey('#control_end', 69);
+setKey('#control_pause', 80);
 
 //console.log('end player.js');
